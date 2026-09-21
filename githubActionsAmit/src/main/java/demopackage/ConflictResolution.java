6c9754086c811ea521f5e92592464616d882fa08
Amit <amitkatti5@gmail.com>
2026-09-21 18:02:41 +0530

    commit

--- githubActionsAmit/src/main/java/demopackage/ConflictResolution.java
+++ githubActionsAmit/src/main/java/demopackage/ConflictResolution.java
@@ -1,7 +1,9 @@
 package demopackage;
 
-public class ConflictResolution {
-	public static void main(String[] args) {
+public class ConflictResolution 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("make chnages below");
 		System.out.println("changes by adwaith");
 	}
